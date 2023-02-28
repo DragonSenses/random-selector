0087764a2c425c0673ab6f8d604027a51e3a2ca8
DragonSenses <kendrickfrancisco@gmail.com>
2023-02-28 15:18:13 -0800

    Create the selection process

--- script.js
+++ script.js
@@ -30,7 +30,77 @@ function createTags(input) {
   })
 }
 
+/**
+ * Highlights the given tag
+ * @param {*} tag to highlight 
+ */
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+
+/**
+ * Removes the highlight of the given tag
+ * @param {*} tag tag to remove highlight
+ */
+function removeHighlightTag(tag) {
+  tag.classList.remove('highlight');
+}
+
+/**
+ * 
+ * @returns a random tag among the set of choices
+ */
+function pickRandomTag() {
+  /* Get the node list of tags */
+  const tags = document.querySelectorAll('.tag');
+  /* Select a random index */
+  return tags[0];
+}
+
+/**
+ * Randomly selects a tag. Highlights tags until the last choice is selected. 
+ */
+function selectRandom(){
+  const highlightInstances = 20; 
+  const highlightTime = 100;   // in milliseconds
+
+  /* The highlight animation that demonstrates the illusion of choice */
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      removeHighlightTag(randomTag);
+    }, highlightTime)
+
+  }, highlightTime);
+
+  /* The actual selection */
+  setTimeout(() => {
+    // Stop the highlight interval
+    clearInterval(interval);
+
+    setTimeout( () => {
+      const chosenOne = pickRandomTag();
+      highlightTag(chosenOne)
+
+    }, highlightTime)
+  }, highlightInstances * highlightTime)
+}
+
 /* Event Listeners */
 textArea.addEventListener('keyup', (e) => {
   createTags(e.target.value);
+
+  /* Check if user hits 'Enter' key */
+  if(e.key === 'Enter') {
+    /* Clear the input after some time */
+    setTimeout( () => {
+      e.target.value = '';
+    }, 10);
+    
+    selectRandom();
+  }
 });
